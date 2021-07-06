1aae86a36013eac6e68340677a77521393801c78
frhl <frederik.heymann@gmail.com>
2021-07-07 00:59:59 +0200

    added more iterations;

--- workflows/210703_shuffle_utrs_5prime.R
+++ workflows/210703_shuffle_utrs_5prime.R
@@ -33,7 +33,7 @@ res_mat_obs <- as.data.frame(do.call(rbind, res_obs))
 colnames(res_mat_obs) <- paste0('obs.',codons)
 res_mat_obs$ensgid_version <- d$ensgid_version
 res_mat_obs$enstid_version <- d$enstid_version
-fwrite(res_mat_obs, 'derived/210707_MANE.GRCh38.v0.95_five_prime_utr_codons_obs.csv', sep = ',')
+fwrite(res_mat_obs, 'derived/210707b_MANE.GRCh38.v0.95_five_prime_utr_codons_obs.csv', sep = ',')
 
 # simulate expected codons given sequence context
 write('simulating expected codons..',stdout())
@@ -44,7 +44,7 @@ res <- lapply(1:replicates, function(i){
   res_expt <- sim_expected_codons(d$seq[interval], k = 2, iter = iterations, codons = codons, parallel = T)
   
   # save confidence intervals
-  outfile_ci <- paste0('derived/210707_MANE.GRCh38.v0.95_five_prime_utr_codons_expt_ci_rep',i,'.csv')
+  outfile_ci <- paste0('derived/210707b_MANE.GRCh38.v0.95_five_prime_utr_codons_expt_ci_rep',i,'.csv')
   res_mat_expt <- as.data.frame(do.call(rbind, res_expt))
   colnames(res_mat_expt) <- paste0('expt.',codons)
   res_mat_expt$ensgid_version <- d$ensgid_version[interval]
@@ -52,7 +52,7 @@ res <- lapply(1:replicates, function(i){
   fwrite(res_mat_expt, outfile_ci, sep = ',')
   
   # save only estimates
-  outfile_est <- paste0('derived/210707_MANE.GRCh38.v0.95_five_prime_utr_codons_expt_rep',i,'.csv')
+  outfile_est <- paste0('derived/210707b_MANE.GRCh38.v0.95_five_prime_utr_codons_expt_rep',i,'.csv')
   mat_split_expt <- as.data.frame(matrixsplit(res_mat_expt, ';', as.numeric, 3))
   mat_split_expt$ensgid_version <- d$ensgid_version[interval]
   mat_split_expt$enstid_version <- d$enstid_version[interval]
